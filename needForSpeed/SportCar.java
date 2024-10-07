b2ff1bc27d643f525e446aff95493321cbe8bb8a
BoryanaPetkova-eng <130145291+BoryanaPetkova-eng@users.noreply.github.com>
2024-10-07 12:14:31 +0300

    Add tasks for exercise

---
+++ needForSpeed/SportCar.java
@@ -0,0 +1,9 @@
+package needForSpeed;
+
+public class SportCar extends Car{
+    private static final double DEFAULT_FUEL_CONSUMPTION = 10.00;
+    public SportCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
